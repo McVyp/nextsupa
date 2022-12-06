3938cbd00c8075d3d6bc4f3769a911185d395f43
McVyp <93302780+McVyp@users.noreply.github.com>
2022-12-06 13:11:28 +0545

    added loading

---
+++ pages/client-side.tsx
@@ -0,0 +1,23 @@
+import {useEffect, useState} from 'react'
+import supabase from '../utils/supabase';
+
+const About = () => {
+  const [data, setData] = useState<any>(null);
+  const[loading, setLoading] = useState<boolean>(true);
+
+  useEffect(()=>{
+    const getData = async() => {
+      const {data, error} = await supabase.from('park').select()
+      console.log({data, error});
+      setLoading(false);
+      setData(data);
+    }
+    getData();
+  },[]);
+
+  return loading? (
+    <p>Loading...</p>
+  ):(<pre>{JSON.stringify(data, null, 2)}</pre>)
+}
+
+export default About
